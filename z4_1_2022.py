738ab90739c064a5d228dd1c801aa40db2001850
Zuzanna Szostakiewicz <zuzannaszostakiewicz@gmail.com>
2023-01-30 23:01:45 +0100

    Poprawiłam, ale nadal niedziała :(

--- z4_1_2022.py
+++ z4_1_2022.py
@@ -23,4 +23,4 @@ def main():
     print("\ndone, check the file to see for yourself!\n")
 
 if __name__ == '__main__':
-    main()
+        main()
--- z4_2_2022.py
+++ z4_2_2022.py
@@ -5,7 +5,7 @@ def main():
     # i liczbe tycx róznych czynnikow pierwszych
     
     filepath = "przyklad.txt"
-    pri_var = []
+    pri_var = [0, 2]
     pri_quan = int(0)
     com_var = [0]
     com_quan = int(0)
@@ -16,38 +16,38 @@ def main():
         a = line
         print(a)
 
-        while a % 2 == 0:
+        while int(a) % 2 == 0:
             print(a)
-            a /= 2
-            if pri_var[len(pri_var) - 1] != 2:
+            a = int(a) / 2
+            if pri_var[len(pri_var)] != 2:
                 pri_var.append(2)
             pri_quan += 1
 
-        while a % 3 == 0:
+        while int(a) % 3 == 0:
             print(a)
-            a /= 3
-            if pri_var[len(pri_var) - 1] != 3:
+            a = int(a) / 3
+            if pri_var[len(pri_var)] != 3:
                 pri_var.appedn(3)
             pri_quan += 1
 
-        while a % 5 == 0:
+        while int(a) % 5 == 0:
             print(a)
-            a /= 5 
-            if pri_var[len(pri_var) - 1] != 5:
+            a = int(a) / 5 
+            if pri_var[len(pri_var)] != 5:
                 pri_var.append(5)
             pri_quan += 1
 
-        while a % 7 == 0:
+        while int(a) % 7 == 0:
             print(a)
-            a /= 7
-            if pri_var[len(pri_var) - 1] != 7:
+            a = int(a) / 7
+            if pri_var[len(pri_var)] != 7:
                 pri_var.append(7)
             pri_quan += 1
 
-        while a % 9 == 0:
+        while int(a) % 9 == 0:
             print(a)
-            a /= 9
-            if pri_var[len(pri_var) - 1] != 9:
+            a = int(a) / 9
+            if pri_var[len(pri_var)] != 9:
                 pri_var.append(9)
             pri_quan += 1
 
@@ -68,5 +68,5 @@ def main():
 
     f.close()
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
